71824a84e6d28f8e72a35956f5da5dae5f83579d
William Copland <William.Copland@YPIILab.YPII.com>
2017-07-03 11:03:10 -0600

    change GetClientById to GetClient

--- test/service-test-client.js
+++ test/service-test-client.js
@@ -8,9 +8,9 @@ var PROTO_PATH = path.join(__dirname, '../src/core/provider-gateway.proto')
 const service_proto = grpc.load(PROTO_PATH).gateway
 const service = new service_proto.ProviderGateway(SERVICE_BINDING, grpc.credentials.createInsecure())
 
-var searchRequest = { searchName: 'getByClientId', searchParams: [{ name: 'clientId', value: '21' }] }
+var searchRequest = { searchName: 'byClientId', searchParams: [{ name: 'clientId', value: '21' }] }
 
-service.getClientById( searchRequest, function (err, client) {
+service.getClient( searchRequest, function (err, client) {
   if(err) return console.log(err)
   console.log(client)
 })
